45d81d191c8a3bd4f20aecee74c6fc004ae18e18
jaeyoung0509 <jaeyoung0509@naver.com>
2020-12-03 15:11:49 +0900

    nav bar added

--- routes/articles.js
+++ routes/articles.js
@@ -3,7 +3,8 @@ const Article = require('./../models/article')
 const router = express.Router()
 
 router.get('/new' ,(req , res) => {
-  res.sender('articles/new', {article : new Article()})
+    res.send('hi')
+  //res.sender('articles/new', {article : new Article()})
 })
 
 router.get('/:id', async(req, res) => {
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express')
-  const mongoose = require('mongoose')
-  
-  const articleRouter = require('./routes/articles')
-  const Article = require('./models/article')
-  const app = express()
-  mongoose.connect('mongodb://developer:developer@207.148.99.250:27017/article_service' ,{
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
-  app.set('view engine' , 'ejs')
-  //app.use(express.unlencoded({ extended : false }))
-  
-  app.get('/',async (req, res) => {
-    const articles = await Article.find().sort({upload_day : 'desc'})
-    res.render('articles/index' , { articles: articles})
-  })
-  
-  app.use('/articles',articleRouter)
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
+const Article = require('./models/article')  //article 모델
+const { post } = require('./routes/articles')
+const app = express()
 
-  app.listen(3000)
+mongoose.connect('mongodb://developer:developer@207.148.99.250:27017/article_service', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+})
+app.set('view engine', 'ejs')
+//app.use(express.unlencoded(d{ extended : false }))
+
+
+
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ upload_day: 'desc' })
+  res.render('articles/index', { articles: articles })
+
+  var searchQuery = createSearchQuery(req , res);
+  var serached_articles = await post.find(searchQuery)
+  .sort('upload_day')
+  .exec();
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(3000)
